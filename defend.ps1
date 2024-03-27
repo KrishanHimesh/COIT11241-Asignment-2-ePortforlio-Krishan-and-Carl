40e628ad7525f9d431c13623c04073db25e0842d
KrishanHimesh <141705132+KrishanHimesh@users.noreply.github.com>
2024-03-27 21:20:20 +1100

    Final code after analysed
    final commit after analysed by the script analyser

--- defend.ps1
+++ defend.ps1
@@ -1,16 +1,16 @@
-﻿<# Filename: defend.ps1 
+﻿<# Filename: defend.ps1
 Author: Krishan Himesh Abeyrathne - 12217274 , Carl Khyncer Bhryn Yurong - 12224629
-Subject: COIT11241 (Cyber Security Technologies) 
+Subject: COIT11241 (Cyber Security Technologies)
 Purpose: Implement safeguard CIS 2.5 Allowlist Authorised Software #>
 
 # Function to pre-test WDAC
 function PreTest {
     # Run the keyfinder
     Start-Process -FilePath "Z:\keyfinder.exe"
-    Write-Host "Unauthorised app Keyfinder is running"
+    Write-Output "Unauthorised app Keyfinder is running"
 
     # Automate checking the Event Viewer logs for Code Integrity records
-    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | where message -Like "*keyfinder*" | Out-Host
+    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | Where-Object message -Like "*keyfinder*" | Out-Host
 }
 
 
@@ -19,7 +19,7 @@ function SetupWDAC {
     $WDACPolicyXMLFile = "C:\Windows\schemas\CodeIntegrity\ExamplePolicies\DefaultWindows_Audit.xml"
     [xml]$WDACPolicy = Get-Content -Path $WDACPolicyXMLFile
 
-    if ($WDACPolicy.SiPolicy.PolicyID -ne $null) {
+    if ($null -ne $WDACPolicy.SiPolicy.PolicyID) {
         $PolicyID = $WDACPolicy.SiPolicy.PolicyID
         $PolicyBinary = "$PolicyID.cip"
     }
@@ -27,17 +27,17 @@ function SetupWDAC {
     ConvertFrom-CIPolicy -XmlFilePath $WDACPolicyXMLFile -BinaryFilePath "C:\users\vagrant\$PolicyBinary"
 
     if ($(hostname) -eq "win11") {
-        $PolicyBinary 
+        $PolicyBinary
         C:\Windows\System32\CiTool.exe --update-policy "C:\Users\vagrant\$PolicyBinary" -json
     }
 
-    Write-Host "WDAC Policy Applied Successfully"
+    Write-Output "WDAC Policy Applied Successfully"
 }
 
 # Function to post-test WDAC
 function PostTest {
     # Check the WInEvent  List log for Intergrity record
-    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | where message -Like "*keyfinder*" | Out-Host
+    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | Where-Object message -Like "*keyfinder*" | Out-Host
 }
 
 # Function to reset WDAC
@@ -46,13 +46,13 @@ function ResetWDAC {
     $SystemCodeIntegrityFolderRoot = $env:windir + "\System32\CodeIntegrity"
     $MultiplePolicyFilePath = "\CiPolicies\Active\" + $PolicyId + ".cip"
     $PolicyPath = $SystemCodeIntegrityFolderRoot + $MultiplePolicyFilePath
-    
-    Write-Host "Attempting to remove $PolicyPath..." 
+
+    Write-Output "Attempting to remove $PolicyPath..."
     if (Test-Path $PolicyPath) {
         Remove-Item -Path $PolicyPath -Force -ErrorAction Continue
     }
-    
-    Write-Host "WDAC Policy Reset Successfully. Please restart your PC or it will restart automatically in 10 serconds"
+
+    Write-Output "WDAC Policy Reset Successfully. Please restart your PC or it will restart automatically in 10 serconds"
     Start-Sleep -Seconds 10
     Restart-Computer -Force -Confirm:$false
 }
@@ -74,6 +74,6 @@ switch ($command) {
         ResetWDAC
     }
     default {
-        Write-Host "Enter the commamd"
+        Write-Output "Enter the commamd"
     }
 }
