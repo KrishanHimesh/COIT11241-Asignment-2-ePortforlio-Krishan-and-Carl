b39abace7984e83906b16830276203809c3ada89
KrishanHimesh <141705132+KrishanHimesh@users.noreply.github.com>
2024-05-22 20:21:56 +1000

    Final Presentation

---
+++ Final Presentation/PowerShell Scripts/remoteFileCopy.ps1
@@ -0,0 +1,13 @@
+﻿<# Filename: remoteFileCopy.ps1
+Author: Krishan Himesh Abeyrathne - 12217274 
+Subject: COIT11241 (Cyber Security Technologies)
+Purpose: Emulation of Remote file copy #>
+
+# remoteFileCopy.ps1
+# Simulate copying a file from a remote server to the local machine
+$source = "\\DC\pslogs\malicious-file.txt"
+$destination = "Z:\malicious-file.txt"
+Write-Host "Source: $source"
+Write-Host "Destination: $destination"
+Copy-Item -Path $source -Destination $destination -Verbose
+
