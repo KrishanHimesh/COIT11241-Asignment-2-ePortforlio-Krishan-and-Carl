c6c83fb73024c1835d9673089cc741078f411a96
KrishanHimesh <141705132+KrishanHimesh@users.noreply.github.com>
2024-05-24 19:00:09 +1000

    Update directory

--- Task 1 - Peer Programming/README.md
+++
@@ -1,3 +0,0 @@
-Please Note that you have to change the location of the Keyfinder.exe to run this code properly. 
-Clone the whole repository to local host and update the path to the Keyfinder.exe
-
---
+++ Task_1_Peer_Programming/defend.ps1
@@ -0,0 +1,81 @@
+﻿<# Filename: defend.ps1
+Author: Krishan Himesh Abeyrathne - 12217274 , Carl Khyncer Bhryn Yurong - 12224629
+Subject: COIT11241 (Cyber Security Technologies)
+Purpose: Implement safeguard CIS 2.5 Allowlist Authorised Software #>
+
+# Function to pre-test WDAC
+function PreTest {
+    # Run the keyfinder
+    Start-Process -FilePath "Z:\keyfinder.exe"
+    Write-Output "Unauthorised app Keyfinder is running"
+
+    # Automate checking the Event Viewer logs for Code Integrity records
+    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | Where-Object message -Like "*keyfinder*" | Out-Host
+}
+
+
+# Function to setup WDAC
+function SetupWDAC {
+    $WDACPolicyXMLFile = "C:\Windows\schemas\CodeIntegrity\ExamplePolicies\DefaultWindows_Audit.xml"
+    [xml]$WDACPolicy = Get-Content -Path $WDACPolicyXMLFile
+
+    if ($null -ne $WDACPolicy.SiPolicy.PolicyID) {
+        $PolicyID = $WDACPolicy.SiPolicy.PolicyID
+        $PolicyBinary = "$PolicyID.cip"
+    }
+
+    ConvertFrom-CIPolicy -XmlFilePath $WDACPolicyXMLFile -BinaryFilePath "C:\users\vagrant\$PolicyBinary"
+
+    if ($(hostname) -eq "win11") {
+        $PolicyBinary
+        C:\Windows\System32\CiTool.exe --update-policy "C:\Users\vagrant\$PolicyBinary" -json
+    }
+
+    Write-Output "WDAC Policy Applied Successfully"
+}
+
+# Function to post-test WDAC
+function PostTest {
+    # Run the keyfinder
+    Start-Process -FilePath "Z:\keyfinder.exe"
+    # Check the WInEvent  List log for Intergrity record
+    Get-WinEvent (Get-WinEvent -ListLog *Integrity*).logname | Where-Object message -Like "*keyfinder*" | Out-Host
+}
+
+# Function to reset WDAC
+function ResetWDAC {
+    $PolicyID = "{e0abda1f-ccf0-468e-8855-3e0f08b02d6a}"
+    $SystemCodeIntegrityFolderRoot = $env:windir + "\System32\CodeIntegrity"
+    $MultiplePolicyFilePath = "\CiPolicies\Active\" + $PolicyId + ".cip"
+    $PolicyPath = $SystemCodeIntegrityFolderRoot + $MultiplePolicyFilePath
+
+    Write-Output "Attempting to remove $PolicyPath..."
+    if (Test-Path $PolicyPath) {
+        Remove-Item -Path $PolicyPath -Force -ErrorAction Continue
+    }
+
+    Write-Output "WDAC Policy Reset Successfully. Please restart your PC or it will restart automatically in 10 serconds"
+    Start-Sleep -Seconds 10
+    Restart-Computer -Force -Confirm:$false
+}
+
+# Main script
+$command = $args[0]
+
+switch ($command) {
+    "pretest" {
+        PreTest
+    }
+    "wdac-setup" {
+        SetupWDAC
+    }
+    "post-test" {
+        PostTest
+    }
+    "reset-WDAC" {
+        ResetWDAC
+    }
+    default {
+        Write-Output "Enter the commamd"
+    }
+}
